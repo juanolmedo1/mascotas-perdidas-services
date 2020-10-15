52d570de4af5b9e3a12e31b484a920fa98f98e1b
juanolmedo1 <juuan.olmedo@hotmail.com>
2020-10-15 18:43:40 -0300

    14-10-2020/Common_values_table (#11)
    * Common values table added
    
    * Node-cron added to update common values table
    
    * Color stored as array

---
+++ src/entity/CommonValues.ts
@@ -0,0 +1,28 @@
+import {
+  Entity,
+  BaseEntity,
+  PrimaryColumn,
+  Column,
+} from "typeorm";
+import { Field, ObjectType } from "type-graphql";
+import { PetGender, PetSize } from "@entity/Pet";
+
+@ObjectType()
+@Entity()
+export class CommonValues extends BaseEntity {
+  @PrimaryColumn()
+  @Field()
+  breed: string;
+
+  @Field()
+  @Column({ type: "enum", enum: PetGender })
+  gender: PetGender;
+
+  @Field()
+  @Column({ type: "enum", enum: PetSize })
+  size: PetSize;
+
+  @Field(() => [String])
+  @Column("simple-array")
+  color: string[];
+}
